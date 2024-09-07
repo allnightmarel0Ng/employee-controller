cbe8459aa73152f7ad10ef37677ea7ddfc912c3d
allnighmarel0Ng <den.vasiliev65@gmail.com>
2024-09-07 17:41:55 +0300

    kafka wrapper implemented

---
+++ internal/infrastructure/kafka/consumer.go
@@ -0,0 +1,39 @@
+package kafka
+
+import (
+	"time"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+type Consumer struct {
+	consumer *kafka.Consumer
+}
+
+func NewConsumer(broker string, group string) (*Consumer, error) {
+	c, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": broker,
+		"group.id":          group,
+		"auto.offset.reset": "earliest",
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &Consumer{
+		consumer: c,
+	}, nil
+}
+
+func (c *Consumer) SubscribeTopics(topics []string) error {
+	return c.consumer.SubscribeTopics(topics, nil)
+}
+
+func (c *Consumer) Consume(timeout time.Duration) (*kafka.Message, error) {
+	return c.consumer.ReadMessage(timeout)
+}
+
+func (c *Consumer) Close() error {
+	return c.consumer.Close()
+}
---
+++ internal/infrastructure/kafka/producer.go
@@ -0,0 +1,28 @@
+package kafka
+
+import "github.com/confluentinc/confluent-kafka-go/kafka"
+
+type Producer struct {
+	producer *kafka.Producer
+}
+
+func NewProducer(broker string) (*Producer, error) {
+	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.service": broker})
+	if err != nil {
+		return nil, err
+	}
+	return &Producer{
+		producer: p,
+	}, nil
+}
+
+func (p *Producer) Produce(topic string, message []byte) error {
+	return p.producer.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+		Value:          message,
+	}, nil)
+}
+
+func (p *Producer) Close() {
+	p.producer.Close()
+}

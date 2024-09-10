ff46453b955b72a65fc4d2afd85ab0185c781f65
allnighmarel0Ng <den.vasiliev65@gmail.com>
2024-09-10 13:22:04 +0300

    main for storage microservice developed

---
+++ cmd/storage/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+
+	"github.com/allnightmarel0Ng/employee-controller/internal/app/storage/handler"
+	"github.com/allnightmarel0Ng/employee-controller/internal/app/storage/repository"
+	"github.com/allnightmarel0Ng/employee-controller/internal/app/storage/usecase"
+	"github.com/allnightmarel0Ng/employee-controller/internal/config"
+	"github.com/allnightmarel0Ng/employee-controller/internal/infrastructure/kafka"
+	"github.com/allnightmarel0Ng/employee-controller/internal/infrastructure/redis"
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	conf, err := config.LoadConfig()
+	if err != nil {
+		log.Fatalf("unable to load config")
+	}
+
+	ctx := context.Background()
+
+	redisClient := redis.NewClient("redis:"+conf.RedisPort, "", 0)
+	defer redisClient.Close()
+
+	repo := repository.NewStorageRepository(redisClient)
+
+	consumer, err := kafka.NewConsumer("broker:"+conf.KafkaBroker, "activities")
+	if err != nil {
+		log.Fatalf("unable to start the kafka consumer")
+	}
+	defer consumer.Close()
+	err = consumer.SubscribeTopics([]string{"events"})
+	if err != nil {
+		log.Fatalf("unable to subscribe consumer on topics")
+	}
+
+	useCase := usecase.NewStorageUseCase(repo)
+	handle := handler.NewStorageHandler(useCase, consumer, ctx)
+
+	go handle.HandleConsumer()
+
+	router := chi.NewRouter()
+	router.Get("/employee/{IP}", handle.GetEmployeeByIP)
+
+	err = http.ListenAndServe(":"+conf.HTTPPort, router)
+	if err != nil {
+		log.Fatalf("unable to start http server")
+	}
+}

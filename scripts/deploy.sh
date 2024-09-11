14eb1e88c7f00331d9cc0a637343eb8de141fe17
allnighmarel0Ng <den.vasiliev65@gmail.com>
2024-09-11 14:18:21 +0300

    adjust deploying script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,14 @@
-docker-compose --file ./deployments/docker-compose.yml build
-docker-compose --file ./deployments/docker-compose.yml up -d zookeeper broker
+#!/bin/bash
 
-until docker-compose --file ./deployments/docker-compose.yml exec broker nc -z localhost 9092; do
+COMPOSE_FILE=./deployments/docker-compose.yml
+
+docker-compose --file $(COMPOSE_FILE) build
+docker-compose --file $(COMPOSE_FILE) up -d zookeeper broker
+
+until docker-compose --file $(COMPOSE_FILE) exec broker nc -z localhost 9092; do
   sleep 1
 done
 
-docker-compose --file ./deployments/docker-compose.yml exec broker kafka-topics --create --if-not-exists --topic events --bootstrap-server broker:9092 --partitions 1 --replication-factor 1
+docker-compose --file $(COMPOSE_FILE) exec broker kafka-topics --create --if-not-exists --topic events --bootstrap-server broker:9092 --partitions 1 --replication-factor 1
 
-docker-compose --file ./deployments/docker-compose.yml up -d redis collector storage
+docker-compose --file $(COMPOSE_FILE) up -d redis collector storage

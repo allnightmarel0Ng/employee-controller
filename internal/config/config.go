ea7b0c536da588648ea302c93e8225ab68ff151f
allnightmarel0Ng <den.vasiliev65@gmail.com>
2024-09-30 16:41:41 +0300

    config is more precise

--- internal/config/config.go
+++ internal/config/config.go
@@ -7,10 +7,15 @@ import (
 )
 
 type Config struct {
-	TCPPort     string
-	KafkaBroker string
-	HTTPPort    string
-	RedisPort   string
+	TCPPort          string
+	KafkaBroker      string
+	HTTPPort         string
+	RedisPort        string
+	ContainerPort    string
+	PostgresPort     string
+	PostgresUser     string
+	PostgresPassword string
+	PostgresName     string
 }
 
 func LoadConfig() (*Config, error) {
@@ -20,9 +25,14 @@ func LoadConfig() (*Config, error) {
 	}
 
 	return &Config{
-		TCPPort:     os.Getenv("TCP_PORT"),
-		KafkaBroker: os.Getenv("KAFKA_BROKER"),
-		HTTPPort:    os.Getenv("HTTP_PORT"),
-		RedisPort:   os.Getenv("REDIS_PORT"),
+		TCPPort:          os.Getenv("TCP_PORT"),
+		KafkaBroker:      os.Getenv("KAFKA_BROKER"),
+		HTTPPort:         os.Getenv("HTTP_PORT"),
+		RedisPort:        os.Getenv("REDIS_PORT"),
+		ContainerPort:    os.Getenv("CONTAINER_PORT"),
+		PostgresPort:     os.Getenv("POSTGRES_PORT"),
+		PostgresUser:     os.Getenv("POSTGRES_USER"),
+		PostgresPassword: os.Getenv("POSTGRES_PASSWORD"),
+		PostgresName:     os.Getenv("POSTGRES_NAME"),
 	}, nil
 }
